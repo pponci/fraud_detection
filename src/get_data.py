e7adf8bb5a8267409ad472c9a8c12c72e00e4720
pponci <132877541+pponci@users.noreply.github.com>
2025-08-21 17:57:59 +0200

    rewrote get data and split data scripts into functions so they can be later used in pipelines

--- src/get_data.py
+++ src/get_data.py
@@ -1,29 +1,37 @@
 import os
-import zipfile
 
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-# config
-dataset = "ealaxi/paysim1"
-raw_dir = "./data/raw"
-target_file = "PS_20174392719_1491204439457_log.csv"
-new_filename = "raw_data.csv"
+def download_dataset():
+    """
+    This function dowloads the dataset from kaggle and renames
+    the file.
+    """
 
-# ensure directory exists
-os.makedirs(raw_dir, exist_ok = True)
+    # config
+    dataset = "ealaxi/paysim1"
+    raw_dir = "./data/raw"
+    target_file = "PS_20174392719_1491204439457_log.csv"
+    new_filename = "raw_data.csv"
 
-# authenticate kaggle api and download dataset
-api = KaggleApi()
-api.authenticate()
+    # ensure directory exists
+    os.makedirs(raw_dir, exist_ok = True)
 
-api.dataset_download_files(dataset, path = raw_dir, unzip = True)
+    # authenticate kaggle api and download dataset
+    api = KaggleApi()
+    api.authenticate()
 
-# rename dataset
-old_path = os.path.join(raw_dir, target_file)
-new_path = os.path.join(raw_dir, new_filename)
+    api.dataset_download_files(dataset, path = raw_dir, unzip = True)
 
-if os.path.exists(old_path):
-    os.rename(old_path, new_path)
+    # rename dataset
+    old_path = os.path.join(raw_dir, target_file)
+    new_path = os.path.join(raw_dir, new_filename)
 
-else:
-    print(f"File {target_file} not found")
+    if os.path.exists(old_path):
+        os.rename(old_path, new_path)
+
+    else:
+        print(f"File {target_file} not found")
+
+if __name__ == "__main__":
+    download_dataset()
--- src/split_data.py
+++ src/split_data.py
@@ -1,16 +1,24 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-seed = 11
+def data_split():
+    """
+    This function loads the raw data and then performs the split in train,
+    validation, and test dataset. After it saves the datasets.
+    """
+    seed = 11
 
-# load raw data
-df = pd.read_csv("./data/raw/raw_data.csv")
+    # load raw data
+    df = pd.read_csv("./data/raw/raw_data.csv")
 
-# split data
-df_train, df_val = train_test_split(df, test_size = 0.3, random_state = seed, stratify = df.isFraud)
-df_val, df_test = train_test_split(df_val, test_size = 1/3, random_state = seed, stratify = df_val.isFraud)
+    # split data
+    df_train, df_val = train_test_split(df, test_size = 0.3, random_state = seed, stratify = df.isFraud)
+    df_val, df_test = train_test_split(df_val, test_size = 1/3, random_state = seed, stratify = df_val.isFraud)
 
-# save split data
-df_train.to_csv("./data/processed/train_data.csv")
-df_val.to_csv("./data/processed/validation_data.csv")
-df_test.to_csv("./data/processed/test_data.csv")
+    # save split data
+    df_train.to_csv("./data/processed/train_data.csv")
+    df_val.to_csv("./data/processed/validation_data.csv")
+    df_test.to_csv("./data/processed/test_data.csv")
+
+if __name__ == "__main__":
+    data_split()
