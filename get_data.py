964e86c7c91e4a49fcb6dd03dc7444b4a282efa2
pponci <132877541+pponci@users.noreply.github.com>
2025-08-21 16:53:26 +0200

    added get data script to download the datset automatically

---
+++ get_data.py
@@ -0,0 +1,29 @@
+import os
+import zipfile
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+# config
+dataset = "ealaxi/paysim1"
+raw_dir = "./data/raw"
+target_file = "PS_20174392719_1491204439457_log.csv"
+new_filename = "raw_data.csv"
+
+# ensure directory exists
+os.makedirs(raw_dir, exist_ok = True)
+
+# authenticate kaggle api and download dataset
+api = KaggleApi()
+api.authenticate()
+
+api.dataset_download_files(dataset, path = raw_dir, unzip = True)
+
+# rename dataset
+old_path = os.path.join(raw_dir, target_file)
+new_path = os.path.join(raw_dir, new_filename)
+
+if os.path.exists(old_path):
+    os.rename(old_path, new_path)
+
+else:
+    print(f"File {target_file} not found")
